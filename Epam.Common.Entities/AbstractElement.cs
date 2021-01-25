062a1285eb28f7782f7e9ebcb769ae32d2a816eb
Danil Tiurin <Danil_Tiurin@epam.com>
2021-01-25 18:34:44 +0400

    EPMRDRUNET-1590 Library Created abstract element in entities

---
+++ Epam.Common.Entities/AbstractElement.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Common.Entities
+{
+    public abstract class AbstractElement
+    {
+        public abstract string Name { get; protected set; }
+        //{
+        //    get => Name;
+
+        //    private set
+        //    {
+        //        Name = value.Length > 300
+        //            ? value.Substring(0, 300)
+        //            : value;
+        //    }
+        //}
+
+        public abstract int NumberOfPages { get; protected set; }
+        //{
+        //    get => NumberOfPages;
+
+        //    private set
+        //    {
+        //        NumberOfPages = value < 0
+        //            ? throw new ArgumentException("The number of pages cannot be negative!")
+        //            : value;
+        //    }
+        //}
+
+        public abstract string Annotation { get; protected set; }
+        //{ 
+        //    get => Annotation;
+
+        //    private set
+        //    {
+        //        Annotation = value.Length > 2000
+        //            ? value.Substring(0, 2000)
+        //            : value;
+        //    }
+        //}
+
+        protected AbstractElement(string name, int numberOfPages, string annotation)
+        {
+            Name = name;
+            NumberOfPages = numberOfPages;
+            Annotation = annotation;
+        }
+        protected AbstractElement(string name, int numberOfPages)
+        {
+            Name = name;
+            NumberOfPages = numberOfPages;
+        }
+    }
+}
