571fba14eba2c74005b56eaa009f0a5a605ad37f
Danil Tiurin <Danil_Tiurin@epam.com>
2021-01-26 15:35:43 +0400

    EPMRDRUNET-1590 Library Create class AbstractAutorsElement

---
+++ Epam.Common.Entities/AutorsElement/AbstractAutorsElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Common.Entities.AutorsElement
+{
+    public abstract class AbstractAutorsElement : AbstractElement 
+    {
+        public Autor Autor { get; private set; }
+
+        public AbstractAutorsElement(string name, int numberOfPages, Autor autor) : base(name, numberOfPages)
+        {
+            Autor = autor;
+        }
+        public AbstractAutorsElement(string name, string annotation, int numberOfPages, Autor autor) : base(name, numberOfPages, annotation)
+        {
+            Autor = autor;
+        }
+    }
+}
