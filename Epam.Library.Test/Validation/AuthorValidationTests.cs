b61747e6691f549ee3075b27cd985c73a57ebcde
Danil Tiurin <Danil_Tiurin@epam.com>
2021-02-10 23:02:34 +0400

    EPMRDRUNET-1642 UnitTests Created AuthorValidationTestCases

--- Epam.Library.Test/Validation/AuthorValidationTests.cs
+++ Epam.Library.Test/Validation/AuthorValidationTests.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Epam.Library.Bll.Validation;
 using Epam.Library.Common.Entities.AuthorElement;
+using Epam.Library.Test.Validation.TestCases;
 using NUnit.Framework;
 
 namespace Epam.Library.Test.Validation
@@ -9,11 +10,8 @@ namespace Epam.Library.Test.Validation
     [TestFixture]
     public class AuthorValidationTests
     {
-        [TestCase("Тест")]
-        [TestCase("Тест-Тест")]
-        [TestCase("Test")]
-        [TestCase("Test-Test")]
-        public void ValidateTrueAuthorFirstNameTest(string value)
+        [TestCaseSource(typeof(AuthorValidationTestCases), nameof(AuthorValidationTestCases.ValidateFirstNameTestCases))]
+        public bool Validate_FirstName(string value)
         {
             Author author = new Author();
 
@@ -21,33 +19,12 @@ namespace Epam.Library.Test.Validation
 
             var result = new AuthorValidation().Validate(author);
 
-            Assert.IsFalse(result.Any(a => a.Field.Equals(nameof(author.FirstName))));
-        }
-
-        [TestCase("тест")]
-        [TestCase("-тест")]
-        [TestCase("тест-")]
-        [TestCase("Тест-тест")]
-        [TestCase("тест-Тест")]
-        [TestCase("-Test")]
-        [TestCase("Test-")]
-        [TestCase("-Test-Test")]
-        [TestCase("Test-Test-")]
-        [TestCase("Test-Тест")]
-        public void ValidateFalseAuthorFirstNameTest(string value)
-        {
-            Author author = new Author();
-
-            author.FirstName = value;
-
-            var result = new AuthorValidation().Validate(author);
-
-            Assert.IsTrue(result.Any(a => a.Field.Equals(nameof(author.FirstName))));
+            return result.Any(a => a.Field.Equals(nameof(author.FirstName)));
         }
 
         [TestCase('t', 50, ExpectedResult = false)]
         [TestCase('t', 51, ExpectedResult = true)]
-        public bool ValidateSizeInAuthorFirstNameTest(char value, int count)
+        public bool Validate_FirstName_Size(char value, int count)
         {
             Author author = new Author();
 
@@ -58,15 +35,8 @@ namespace Epam.Library.Test.Validation
             return result.Any(a => a.Field.Equals(nameof(author.FirstName)));
         }
 
-        [TestCase("Тест")]
-        [TestCase("тест Тест")]
-        [TestCase("Тест-Тест")]
-        [TestCase("Тест'Тест")]
-        [TestCase("Test")]
-        [TestCase("test Test")]
-        [TestCase("Test-Test")]
-        [TestCase("Test'Test")]
-        public void ValidateTrueAuthorLastNameTest(string value)
+        [TestCaseSource(typeof(AuthorValidationTestCases), nameof(AuthorValidationTestCases.ValidateLastNameTestCases))]
+        public bool Validate_LastName(string value)
         {
             Author author = new Author();
 
@@ -74,39 +44,7 @@ namespace Epam.Library.Test.Validation
 
             var result = new AuthorValidation().Validate(author);
 
-            Assert.IsFalse(result.Any(a => a.Field.Equals(nameof(author.LastName))));
-        }
-
-        [TestCase("тест")]
-        [TestCase("-тест")]
-        [TestCase("тест-")]
-        [TestCase("'тест")]
-        [TestCase("тест'")]
-        [TestCase("Тест Тест")]
-        [TestCase("тест-Тест")]
-        [TestCase("Тест-тест")]
-        [TestCase("Тест'тест")]
-        [TestCase("тест'Тест")]
-        [TestCase("test")]
-        [TestCase("-test")]
-        [TestCase("test-")]
-        [TestCase("'test")]
-        [TestCase("test'")]
-        [TestCase("Test Test")]
-        [TestCase("test-Test")]
-        [TestCase("Test-test")]
-        [TestCase("Test'test")]
-        [TestCase("test'Test")]
-        [TestCase("Тест-Test")]
-        public void ValidateFalseAuthorLastNameTest(string value)
-        {
-            Author author = new Author();
-
-            author.LastName = value;
-
-            var result = new AuthorValidation().Validate(author);
-
-            Assert.IsTrue(result.Any(a => a.Field.Equals(nameof(author.LastName))));
+            return result.Any(a => a.Field.Equals(nameof(author.LastName)));
         }
 
         [TestCase('t', 200, ExpectedResult = false)]
