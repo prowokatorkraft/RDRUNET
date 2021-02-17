9b0650707a86d285b483763f06f09d8209b6d7e9
Danil Tiurin <Danil_Tiurin@epam.com>
2021-02-17 11:36:59 +0400

    EPMRDRUNET-1642 IntegrationTests Finished AuthorBllIntegrationTests

--- IntegrationTest/AuthorBllIntegrationTests.cs
+++ IntegrationTest/AuthorBllIntegrationTests.cs
@@ -8,92 +8,125 @@ using Epam.Library.Common.Entities.AuthorElement;
 using Epam.Library.Common.Entities.AuthorElement.Patent;
 using Epam.Library.Common.Entities.Exceptions;
 using Epam.Library.Common.Entities.SearchOptionsEnum;
+using IntegrationTest.TestCases;
 using NUnit.Framework;
 
 namespace Epam.Library.IntegrationTest
 {
-    [TestFixture]
+
     public class AuthorBllIntegrationTests
     {
         private IAuthorBll _authorBll;
-
         private ICatalogueBll _catalogueBll;
+        private IPatentBll _patentBll;
 
-        [TestFixtureSetUp]
+        private List<int> _authorIDs;
+        private List<int> _patentIDs;
+
+        [OneTimeSetUp]
         public void InitClass()
         {
             _authorBll = DependencyInjection.AuthorBll;
             _catalogueBll = DependencyInjection.CatalogueBll;
+            _patentBll = DependencyInjection.PatentBll;
+
+            _authorIDs = new List<int>();
+            _patentIDs = new List<int>();
+        }
+
+        [OneTimeTearDown]
+        public void DiposeClass()
+        {
+            _patentIDs.ForEach(p => _patentBll.Remove(p));
+
+            _authorIDs.ForEach(a => _authorBll.Remove(a));
         }
 
         [Test]
         public void Add_True()
         {
             // Arrange
+            Author author = new Author(null, "Add", "True");
             int preCount = GetCount();
-            Author author = new Author(null, "Test", "Test");
+            int id;
 
             // Act
             var errors = _authorBll.Add(author);
+            _authorIDs.Add(id = GetId(author).Value);
             int postCount = GetCount();
 
-            int? id = GetId(author);
-            if (id.HasValue)
-            {
-                _authorBll.Remove(id.Value);
-            }
-
             // Assert
-            Assert.AreEqual(0, errors.Count());
-            Assert.AreEqual(preCount + 1, postCount);
-            Assert.IsTrue(id.HasValue);
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(0, errors.Count());
+                Assert.AreEqual(preCount + 1, postCount);
+            });
         }
 
         [Test]
         public void Add_False()
         {
             // Arrange
+            Author author = new Author(null, null, null);
             int preCount = GetCount();
-            Author author = new Author(null, "Test", null);
 
             // Act
             var errors = _authorBll.Add(author);
+            int postCount = GetCount();
 
             // Assert
-            Assert.IsTrue(errors.Count() > 0);
-            Assert.AreEqual(preCount, GetCount());
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(errors.Count() > 0);
+                Assert.AreEqual(preCount, postCount);
+            });
         }
 
         [Test]
         public void Add_Null_Exception()
         {
             // Arrange
-            int count = GetCount();
+            int preCoutn = GetCount();
 
             // Act
             TestDelegate test = () => _authorBll.Add(null);
 
             // Assert
-            Assert.Throws<AddException>(test);
-            Assert.AreEqual(count, GetCount());
+            Assert.Multiple(() =>
+            {
+                Assert.Throws<AddException>(test);
+                Assert.AreEqual(preCoutn, GetCount());
+            });
         }
 
         [Test]
         public void Remove_True()
         {
             // Arrange
-            Author author = new Author(null, "Test", "Test");
+            Author author = new Author(null, "Remove", "True");
+
             _authorBll.Add(author);
 
-            int preCount = GetCount();
             int id = GetId(author).Value;
+
+            int preCount = GetCount();
 
             // Act
             bool isRemoved = _authorBll.Remove(id);
 
+            int postCount = GetCount();
+
+            if (!isRemoved || preCount == postCount)
+            {
+                _authorIDs.Add(id);
+            }
+
             // Assert
-            Assert.IsTrue(isRemoved);
-            Assert.AreEqual(preCount - 1, GetCount());
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(isRemoved);
+                Assert.AreEqual(preCount - 1, postCount);
+            });
         }
 
         [Test]
@@ -106,57 +139,127 @@ namespace Epam.Library.IntegrationTest
             TestDelegate test = () => _authorBll.Remove(-30000);
 
             // Assert
-            Assert.Throws<RemoveException>(test);
-            Assert.AreEqual(preCount, GetCount());
+            Assert.Multiple(() =>
+            {
+                Assert.Throws<RemoveException>(test);
+                Assert.AreEqual(preCount, GetCount());
+            });
         }
 
         [Test]
         public void Remove_Dependencies_False()
         {
             // Arrange
-            Author author = new Author(null, "Test", "Test");
+            Author author = new Author(null, "Remove", "Dependencies");
             _authorBll.Add(author);
-            int preCount = GetCount();
-            int idAuthor = GetId(author).Value;
 
-            Patent patent = new Patent(null, "Test", 0, null, new int[] { idAuthor }, "Test", "123456789", null, DateTime.Now);
-            DependencyInjection.PatentBll.Add(patent);
-            int idPatent = GetId(patent).Value;
+            int preCount = GetCount();
+            int idAuthor;
+
+            _authorIDs.Add(idAuthor = GetId(author).Value);
+
+            Patent patent = new Patent(null, "Remove Dependencies", 0, null, new int[] { idAuthor }, "Test", "123456789", null, DateTime.Now);
+            _patentBll.Add(patent);
+
+            int idPatent;
+            _patentIDs.Add(idPatent = GetId(patent).Value);
 
             // Act
             bool isRemoved = _authorBll.Remove(idAuthor);
 
             // Assert
-            Assert.IsFalse(isRemoved);
-            Assert.AreEqual(preCount, GetCount());
-
-            DependencyInjection.PatentBll.Remove(idPatent);
-            _authorBll.Remove(idAuthor);
+            Assert.Multiple(() =>
+            {
+                Assert.IsFalse(isRemoved);
+                Assert.AreEqual(preCount, GetCount());
+            });
         }
 
         [Test]
         public void Get()
         {
             // Arrange
-            Author author = new Author(null, "Test", "Test");
+            Author author = new Author(null, "Get", "Get");
             _authorBll.Add(author);
-            int id = GetId(author).Value;
+
+            int id;
+            _authorIDs.Add(id = GetId(author).Value);
 
             // Act
             Author element = _authorBll.Get(id);
 
-            _authorBll.Remove(id);
-
             // Assert
-            Assert.IsNotNull(element);
-            Assert.AreEqual(author, element);
+            Assert.Multiple(() =>
+            {
+                Assert.IsNotNull(element);
+                Assert.AreEqual(author, element);
+            });
         }
 
         [Test]
-        [ExpectedException(typeof(GetException))]
         public void Get_Exception()
         {
-            _authorBll.Get(-30000);
+            // Arrange
+            TestDelegate test = () => _authorBll.Get(-30000);
+
+            // Assert
+            Assert.Throws<GetException>(test);
+        }
+
+        [Test]
+        public void Check_True()
+        {
+            // Arrange
+            Author[] authors = new Author[]
+            {
+                new Author(null, "Check", "True-One"),
+                new Author(null, "Check", "True-Two"),
+                new Author(null, "Check", "True-Three")
+            };
+
+            List<int> ids = new List<int>();
+
+            foreach (var item in authors)
+            {
+                _authorBll.Add(item);
+                ids.Add(GetId(item).Value);
+            }
+
+            _authorIDs.AddRange(ids);
+
+            // Act
+            var result = _authorBll.Check(ids.ToArray());
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Check_False()
+        {
+            // Act
+            var result = _authorBll.Check(new int[] { -30000 });
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestCaseSource(typeof(AuthorBllIntegrationTestCases), nameof(AuthorBllIntegrationTestCases.Search))]
+        public bool Search(Author author, SearchRequest<SortOptions, AuthorSearchOptions> request)
+        {
+            // Arrange
+            if (author != null)
+            {
+                _authorBll.Add(author);
+
+                _authorIDs.Add(GetId(author).Value);
+            }
+
+            //Act
+            bool result = _authorBll.Search(request).Any();
+
+            // Assert
+            return result;
         }
 
         private int GetCount()
---
+++ IntegrationTest/TestCases/AuthorBllIntegrationTestCases.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using Epam.Library.Common.Entities;
+using Epam.Library.Common.Entities.AuthorElement;
+using Epam.Library.Common.Entities.SearchOptionsEnum;
+using NUnit.Framework;
+
+namespace IntegrationTest.TestCases
+{
+    public static class AuthorBllIntegrationTestCases
+    {
+        public static IEnumerable Search
+        {
+            get
+            {
+                yield return new TestCaseData
+                (
+                    new Author(null, "Search", "Null"),
+                    null
+                ).Returns(true);
+
+                yield return new TestCaseData
+                (
+                    new Author(null, "Test-One", "Test-One"), 
+                    new SearchRequest<SortOptions, AuthorSearchOptions>(SortOptions.None, AuthorSearchOptions.None, null)
+                ).Returns(true);
+
+                yield return new TestCaseData
+                (
+                    new Author(null, "First-Name", "Search"),
+                    new SearchRequest<SortOptions, AuthorSearchOptions>(SortOptions.None, AuthorSearchOptions.FirstName, "First-Name")
+                ).Returns(true);
+
+                yield return new TestCaseData
+                (
+                    null,
+                    new SearchRequest<SortOptions, AuthorSearchOptions>(SortOptions.None, AuthorSearchOptions.FirstName, "--------")
+                ).Returns(false);
+
+                yield return new TestCaseData
+                (
+                    null,
+                    new SearchRequest<SortOptions, AuthorSearchOptions>(SortOptions.None, AuthorSearchOptions.LastName, "--------")
+                ).Returns(false);
+            }
+        }
+    }
+}
